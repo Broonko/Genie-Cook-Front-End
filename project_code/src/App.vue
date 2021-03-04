83d8d0721f76ff7df4042800bd56326e4b87f841
Rosana <rosanartec@gmail.com>
2021-03-04 02:44:47 +0000

    cards created

--- project_code/src/App.vue
+++ project_code/src/App.vue
@@ -1,58 +1,126 @@
 <template>
   <v-app>
-    
-      <v-card color="grey-4" flat height="200px" tile>
-        <v-toolbar  prominent dense dark>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-row>
+      <v-col>
+        <v-card color="grey-4" flat height="200px" tile>
+          <v-toolbar
+            color="light grey"
+            prominent
+            dense
+            class="grey lighten-3 blue--text"
+            app
+          >
+            <v-icon
+              class="mt-8 "
+              color="blue"
+              large
+              dark
+              v-bind="attrs"
+              v-on="on"
+            >
+              mdi-home
+            </v-icon>
 
-          <v-toolbar-title>Title</v-toolbar-title>
+            <v-spacer></v-spacer>
 
-          <v-spacer></v-spacer>
+            <v-tabs justify="center">
+              <v-spacer></v-spacer>
+              <v-tab>E-commerce</v-tab>
+              <v-tab>Tips</v-tab>
 
-          <v-btn icon>
-            <v-icon>mdi-magnify</v-icon>
-          </v-btn>
+              <v-spacer></v-spacer>
+            </v-tabs>
+            <v-spacer></v-spacer>
 
-          <v-btn icon>
-            <v-icon>mdi-heart</v-icon>
-          </v-btn>
+            <v-btn icon>
+              <v-icon class="blue--text">mdi-magnify</v-icon>
+            </v-btn>
 
-          <v-btn icon>
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </v-toolbar>
-      </v-card>
-      <v-main>
-        <v-row justify="center">
-      <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field 
-            v-model="search" 
+            <v-btn icon>
+              <v-icon class="blue--text">mdi-dots-vertical</v-icon>
+            </v-btn>
+          </v-toolbar>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-main>
+      <v-row justify="center">
+        <v-col cols="12" sm="6" md="3">
+          <v-text-field
+            v-model="search"
             label="search"
             placeholder="Introduce your ingredients"
             outlined
-            
+            rounded
           ></v-text-field>
-          <v-btn block @click="getRecipes()">search </v-btn>
+          <v-btn rounded block @click="getRecipes()">search</v-btn>
         </v-col>
-        </v-row>
-        
-     
-        <v-row>
-          <v-col class="d-flex" cols="4" v-for="(list, idx) in recipeList" :key="idx">{{ list.title }} {{ list.id }} {{}}
-            <v-img height="200" width="200" :src="list.image"></v-img>
-            </v-col>
-            </v-row>
-          
+      </v-row>
 
+      <!--<v-row>
+        <v-col
+          class="d-flex"
+          cols="4"
+          v-for="(list, idx) in recipeList"
+          :key="idx"
+          >{{ list.title }} id:{{ list.id }}
+          <v-img
+            class="d-flex"
+            height="200"
+            width="200"
+            :src="list.image"
+          ></v-img>
+        </v-col>
+      </v-row>-->
+
+      <v-row row wrap>
+        <v-col>
+          <v-card class="d-flex"
+          cols="4"
+          v-for="(list, idx) in recipeList"
+          :key="idx">
+          <v-img
+            
+            height="250"
+            width="250"
+            :src="list.image"
+          ></v-img>
+
+          <v-card-title>{{ list.title }}</v-card-title>
+
+          <v-card-text>
+            <v-row class="d-flex " align="center" >
+              <v-rating
+                :value="4.5"
+                color="amber"
+                dense
+                half-increments
+                readonly
+                size="14"
+              ></v-rating>
+
+              <div class="grey--text ml-4">
+                4.5 (413)
+              </div>
+            </v-row>
+
+
+            <div>
+              Small plates, salads & sandwiches - an intimate setting with 12
+              indoor seats plus patio seating.
+            </div>
+          </v-card-text>
+
+          <v-divider class="mx-4"></v-divider>
+          </v-card>
+          
+        </v-col>
+      </v-row>
     </v-main>
 
-    <v-footer padless dark>
+    <v-footer class="grey lighten-3" color="light grey">
       <v-col class="text-center" cols="12">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        {{ new Date().getFullYear() }} — <strong>GenieCook</strong>
       </v-col>
     </v-footer>
   </v-app>
@@ -74,9 +142,8 @@ export default {
     };
   },
   props: {
-    search: String
+    search: String,
   },
-
 
   /*mounted() {
     recipesService.getAllRecipes().then((response) => {
