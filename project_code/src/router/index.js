c0d9cf13cb28ed2915bb35ef9bfc580910750d41
Zeben <yusnebez@gmail.com>
2021-03-05 20:20:37 +0000

    signup

--- project_code/src/router/index.js
+++ project_code/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Search from "../views/Search.vue";
 import Home from "../views/Home.vue";
+import Signup from  "../views/Signup.vue";
 
 Vue.use(VueRouter);
 
@@ -17,6 +18,16 @@ const routes = [
     component: Search,
     props: true,
   },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
+  },
+  // {
+  //   path: "/",
+  //   name: "Home",
+  //   component: Home,
+  // },
   /*{
     path: "/about",
     name: "About",
