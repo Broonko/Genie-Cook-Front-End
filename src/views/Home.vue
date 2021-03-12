953f5cb31722d744e20f2d6f10bdb99962a7850e
Rosana <rosanartec@gmail.com>
2021-03-12 19:21:00 +0000

    Caroussel style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,27 +4,31 @@
 
     <v-container class="imgBack">
       <v-row justify="center">
-        <v-col cols="12" sm="4" md="5">
-          <v-text-field
-            flat
-            background-color="white"
-            prepend-inner-icon="mdi-magnify"
-            color="black"
-            v-model="search"
-            label="search"
-            placeholder="Introduce your ingredients"
-            outlined
-            rounded
-          ></v-text-field>
-          <v-btn
-            color="teal lighten-1"
-            dark
-            rounded
-            block
-            :to="{ name: 'Recipes', params: { search: search } }"
-            >search</v-btn
-          >
+        <v-col cols="12" sm="4" md="4">
           <div>
+            <v-text-field
+              flat
+              background-color="white"
+              prepend-inner-icon="mdi-magnify"
+              color="black"
+              v-model="search"
+              label="search"
+              placeholder="Introduce your ingredients"
+              outlined
+              rounded
+            ></v-text-field>
+            <v-btn
+              color="teal lighten-1"
+              dark
+              rounded
+              block
+              :to="{ name: 'Recipes', params: { search: search } }"
+              >search</v-btn
+            >
+          </div>
+          <br />
+          <br />
+          <v-row class=" d-flex" align="center" height="auto" width="auto">
             <v-carousel>
               <v-carousel-item
                 v-for="(item, i) in items"
@@ -34,7 +38,7 @@
                 transition="fade-transition"
               ></v-carousel-item>
             </v-carousel>
-          </div>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>
@@ -48,13 +52,13 @@ export default {
       search: "",
       items: [
         {
-          src: "../assets/lampara.png"
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
         },
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg"
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
         },
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg"
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
         }
       ]
     };
@@ -65,6 +69,7 @@ export default {
 <style lang="scss" scoped>
 .backgr {
   filter: hue-rotate(20deg);
+  display: flex;
   position: relative;
   display: flex;
   height: 100%;
@@ -72,10 +77,11 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  filter: brightness(90%);
+  filter: brightness(85%);
 }
 .imgBack {
   display: flex;
+  align-items: center;
   position: absolute;
   top: 50%;
   left: 50%;
